c79cbce2e940a8126259235159b0adf181b20811
takumi <tkm0703@icloud.com>
2021-03-24 19:56:32 +0900

    add user_show_page image_icon

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,21 @@
 class UserController < ApplicationController
   def new
-    
+    @user = User.new
   end
+  def create
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      image_icon: "mateo-avila-chinchilla-x_8oJhYU31k-unsplash.jpg"
+    )
+    @user.save
+    redirect_to("/users/#{@user.id}")
+  end
+
   def login
   end
   def show
+    @user = User.find_by(id: params[:id])
   end
 end
