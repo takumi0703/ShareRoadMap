7ae57a3f8a3f11239b9d6b9cdbd7f6c49be1cd5c
takumi <tkm0703@icloud.com>
2021-03-24 20:17:36 +0900

    create signup func

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  #ユーザー登録・ログイン
+  #ユーザー登録
   get '/' => "user#new"
+  post 'users/create' => "user#create"
+  #ログイン
   get 'login' => "user#login"
   #ユーザー詳細ページ
-  get 'users/show' => "user#show"
+  get 'users/:id' => "user#show"
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
