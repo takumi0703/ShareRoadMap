3ab488238acedc35dcd4795b407bf400a9531534
takumi <tkm0703@icloud.com>
2021-03-24 17:13:20 +0900

    Create login Page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,4 +2,7 @@ class UserController < ApplicationController
   def signup
     
   end
+  def login
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/' => "user#signup"
+  get 'login' => "user#login"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
