d6281111b7e90a01618ed2eae2b972c4a596f566
takumi <tkm0703@icloud.com>
2021-03-25 15:24:22 +0900

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
  has_secure_password
 
- validates :name, presence: true
- validates :email, presence: true
- validates :password, presence: true
+ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+ validates :name, presence: true,uniqueness: true
+ validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness:true
+ validates :password, presence: true,length: { minimum: 6 }
 end
