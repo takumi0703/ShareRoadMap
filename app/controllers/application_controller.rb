22db3faac52c73131c84704d2d33200b6526b80e
takumi <tkm0703@icloud.com>
2021-03-26 16:21:31 +0900

    create current_user Access restrictions(制限)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  #呼び出し。全てのコントローラで適用される
+  before_action :set_current_user
+  before_action :autheniticate_user,{only: [:show]}
+
+  #ログインユーザー
+  def set_current_user
+   @current_user = User.find_by(id: session[:user_id]) 
+  end
+
+  #未ログインのアクセス制限
+  def autheniticate_user
+   if @current_user==nil
+     flash[:alert] = "アクセス権限がありません"
+     redirect_to("/login")
+   end
+ end
 end
