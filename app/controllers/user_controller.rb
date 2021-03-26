8480cf31d05480c7c440661f4532bc00e54bac4c
takumi <tkm0703@icloud.com>
2021-03-26 16:23:01 +0900

    create logout

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,5 @@
 class UserController < ApplicationController
+  
   def new
     @user = User.new
   end
@@ -11,6 +12,7 @@ class UserController < ApplicationController
       image_icon: "mateo-avila-chinchilla-x_8oJhYU31k-unsplash.jpg"
     )
     if @user.save
+      session[:user_id] = @user.id
       redirect_to("/users/#{@user.id}")
     else
       render("user/new")
@@ -22,6 +24,8 @@ class UserController < ApplicationController
   def login
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      flash[:success] = "ログインしました"
       redirect_to("/users/#{@user.id}")  
     else
       @error_message = "メールアドレスまたはパスワードが間違っています"
@@ -29,8 +33,14 @@ class UserController < ApplicationController
       @password = params[:password]
       render("user/login_form")  
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to("/login")
 
   end
+
   def show
     @user = User.find_by(id: params[:id])
   end
