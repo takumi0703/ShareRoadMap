f8f91d47db7a0fa6797db80d83c79a95e9cbd359
takumi <tkm0703@icloud.com>
2021-03-27 12:58:26 +0900

    add user-image dir

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -61,6 +61,7 @@ class UserController < ApplicationController
       #フォルダに保存
       File.binwrite("public/image/#{@user.image_icon}",File.read(image))
     end
+
     if @user.save
      flash[:success] ="ユーザー情報を編集できました！"
      redirect_to("/users/#{@user.id}")
