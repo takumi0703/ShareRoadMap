6b9502baa28bf597ec3c7303c810ae449feff98b
takumi <tkm0703@icloud.com>
2021-04-01 19:33:36 +0900

    create road show_page

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   get 'users/:id/edit' => "user#edit"
   post 'users/:id/update' => "user#update"
 
+  get  'road/:id' => "road#show"
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
