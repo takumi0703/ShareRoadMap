b793b246f44779c638ed8cdf2de27a429751c468
takumi <tkm0703@icloud.com>
2021-04-01 23:07:22 +0900

    The link to road / show passed

--- app/controllers/road_controller.rb
+++ app/controllers/road_controller.rb
@@ -1,8 +1,11 @@
 class RoadController < ApplicationController
 
   def show
+    #ユーザー情報
     @user = User.find_by(id: params[:id])
-    @roads = Road.where(user_id: @user)
+    #user_idのgoalを取得
+    @roads = Road.find_by(user_id: @user)
+    @studies = Study.where(user_id: @user)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   get 'users/:id/edit' => "user#edit"
   post 'users/:id/update' => "user#update"
 
-  get  'goal/:id' => "goal#show"
+  get  'road/:id' => "road#show"
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
