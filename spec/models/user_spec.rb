b72256b605ab08f05b191a3bef5570c0cf695e6f
takumi <tkm0703@icloud.com>
2021-04-15 20:40:27 +0900

    factoryの導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,31 +4,28 @@ RSpec.describe User, type: :model do
     describe "user新規登録" do
         context "全ての値が正しく入力されている時" do
             it "有効な状態であること" do
-                user =User.new(
-                    name:"hoge",
-                    email:"hoge@example.com",
-                    password:"password",
-                )
+                user = FactoryBot.build(:user)
                 expect(user).to be_valid
             end
         end
         context "nameが空の時" do
             it "nameにエラーが含まれていること" do
-                user = User.new(name: nil)
+                user = FactoryBot.build(:user,name: nil)
+                # user = User.new(name: nil)
                 user.valid?
                 expect(user.errors[:name]).to include("を入力してください")
             end
         end
         context "emailが空の時" do
             it "emailにエラーが含まれていること" do
-                user = User.new(email: nil)
+                user = FactoryBot.build(:user,email: nil)
                 user.valid?
                 expect(user.errors[:email]).to include("を入力してください")
             end
         end
         context "passwordが空の時" do
             it "nameにエラーが含まれていること" do
-                user = User.new(password: nil)
+                user = FactoryBot.build(:user,password: nil)
                 user.valid?
                 expect(user.errors[:password]).to include("を入力してください")
             end
