2152c1555ed093ee9cdb606f36828f8b83973950
takumi <tkm0703@icloud.com>
2021-04-16 01:08:51 +0900

    user spec リファクタリング

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,34 +2,44 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
     describe "user新規登録" do
-        context "全ての値が正しく入力されている時" do
-            it "有効な状態であること" do
-                user = FactoryBot.build(:user)
-                expect(user).to be_valid
+        context "正常な値の時" do
+            context "全ての値が正しく入力されている時" do
+                it "有効な状態であること" do
+                    user = FactoryBot.build(:user)
+                    expect(user).to be_valid
+                end
             end
         end
-        context "nameが空の時" do
-            it "nameにエラーが含まれていること" do
-                user = FactoryBot.build(:user,name: nil)
-                # user = User.new(name: nil)
-                user.valid?
-                expect(user.errors[:name]).to include("を入力してください")
+        context "不正な値の時" do
+            context "nameが空の時" do
+                it "nameにエラーが含まれていること" do
+                    user = FactoryBot.build(:user,name: nil)
+                    user.valid?
+                    expect(user.errors[:name]).to include("を入力してください")
+                end
             end
-        end
-        context "emailが空の時" do
-            it "emailにエラーが含まれていること" do
-                user = FactoryBot.build(:user,email: nil)
-                user.valid?
-                expect(user.errors[:email]).to include("を入力してください")
+            context "emailが空の時" do
+                it "emailにエラーが含まれていること" do
+                    user = FactoryBot.build(:user,email: nil)
+                    user.valid?
+                    expect(user.errors[:email]).to include("を入力してください")
+                end
             end
-        end
-        context "passwordが空の時" do
-            it "nameにエラーが含まれていること" do
-                user = FactoryBot.build(:user,password: nil)
-                user.valid?
-                expect(user.errors[:password]).to include("を入力してください")
+            context "passwordが空の時" do
+                it "nameにエラーが含まれていること" do
+                    user = FactoryBot.build(:user,password: nil)
+                    user.valid?
+                    expect(user.errors[:password]).to include("を入力してください")
+                end
             end
+            # context "nameが重複している時" do
+                # it "nameにエラーが含まれていることと" do
+                #     FactoryBot.create(:user, name: "username")
+                #     user = FactoryBot.build(:user, name: "username")
+                #     user.valid?
+                #     expect(user.errors[:name]).to include("はすでに存在しています")
+                # end
+            # end
         end
     end
-
 end
