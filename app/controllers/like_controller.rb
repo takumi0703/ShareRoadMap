add9cd846f17eed50729dce41166ac407da76fb4
takumi <tkm0703@icloud.com>
2021-04-21 23:27:05 +0900

    like リンク先修正

--- app/controllers/like_controller.rb
+++ app/controllers/like_controller.rb
@@ -2,12 +2,16 @@ class LikeController < ApplicationController
   def create
     like = @current_user.likes.build(study_id: params[:study_id])
     like.save
-    redirect_to("/user/road/#{@current_user.id}")
+    study = Study.find_by(id: params[:study_id])
+    #likeのstudy_idと同じstudyのidをとってくる
+    #study.idを持っているuserをとってくる
+    redirect_to("/user/road/#{study.user_id}")
   end
 
   def destroy
     like = Like.find_by(study_id: params[:study_id],user_id: @current_user.id)
     like.delete
-    redirect_to("/user/road/#{@current_user.id}")
+    study = Study.find_by(id: params[:study_id])
+    redirect_to("/user/road/#{study.user_id}")
   end
 end
