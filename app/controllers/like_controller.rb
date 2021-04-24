92c2420c9bd456058012630f43e91fe67562ffcf
takumi <tkm0703@icloud.com>
2021-04-24 21:44:02 +0900

    いいね機能同期処理に変更

--- app/controllers/like_controller.rb
+++ app/controllers/like_controller.rb
@@ -1,15 +1,15 @@
 class LikeController < ApplicationController
-  before_action :study_params
   def create
-    Like.create(user_id: @current_user.id, study_id: params[:study_id])
+    study = Study.find_by(id: params[:study_id])
+    like = @current_user.likes.build(study_id: params[:study_id])
+    like.save
+    redirect_to("/user/road/#{study.user_id}")
   end
 
   def destroy
-    Like.find_by(user_id: @current_user.id, study_id: params[:study_id]).destroy
-  end
-
-  private
-  def study_params
-    @study = Study.find_by(params[:user_id])
+    study = Study.find_by(id: params[:study_id])
+    like = Like.find_by(study_id: params[:study_id],user_id: @current_user.id)
+    like.delete
+    redirect_to("/user/road/#{study.user_id}")
   end
 end
