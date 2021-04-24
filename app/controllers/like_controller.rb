f4cf892f6b4784a6dde83ce49e48ed4d46a28101
takumi <tkm0703@icloud.com>
2021-04-24 12:53:44 +0900

    不要なコメントを削除

--- app/controllers/like_controller.rb
+++ app/controllers/like_controller.rb
@@ -1,20 +1,5 @@
 class LikeController < ApplicationController
   before_action :study_params
-
-  # def create
-  #   @study = Study.find_by(id: params[:study_id])
-  #   like = @current_user.likes.build(study_id: params[:study_id])
-  #   like.save
-  #   redirect_to("/user/road/#{@study.user_id}")
-  # end
-
-  # def destroy
-  #   study = Study.find_by(id: params[:study_id])
-  #   like = Like.find_by(study_id: params[:study_id],user_id: @current_user.id)
-  #   like.delete
-  #   redirect_to("/user/road/#{@study.user_id}")
-  # end
-
   def create
     Like.create(user_id: @current_user.id, study_id: params[:study_id])
   end
