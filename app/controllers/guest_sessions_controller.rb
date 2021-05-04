eae22051d9836ce2502b853c9d3a0fce32a28e17
takumi <tkm0703@icloud.com>
2021-05-04 12:44:24 +0900

    バグ修正

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -6,7 +6,7 @@ class GuestSessionsController < ApplicationController
       User.find_or_create_by!(email: 'guest@example.com') do |user|
         user.password = SecureRandom.urlsafe_base64
         user.name = 'guest'
-      end
+    end
     session[:user_id] = user.id
     flash[:success] = 'ゲストユーザーとしてログインしました'
     redirect_to user_path(user)
