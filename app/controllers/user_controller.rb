638458c5edb244937a43fb18d45451d23272693b
takumi <tkm0703@icloud.com>
2021-05-05 19:27:24 +0900

    ロードマップ一覧バグ修正

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -75,7 +75,7 @@ class UserController < ApplicationController
 
   def roadshow
     @user = User.find(params[:id])
-    @studies = Study.where(user_id: @user)
+    @studies = Study.where(user_id: @user).order(id: "ASC")
   end
 
   private
