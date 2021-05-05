7a7fceb36f4ea0bd626e5e9b596a0685795e50e5
takumi <tkm0703@icloud.com>
2021-05-05 22:27:36 +0900

    studyモデルにスコープを使用

--- app/models/study.rb
+++ app/models/study.rb
@@ -4,6 +4,8 @@ class Study < ApplicationRecord
   validates :content, presence: true
   validates :user_id, presence: true
   has_many :likes
+  scope :sorted, -> { order(id: :asc) }
+
   def liked_by?(user)
     likes.where(user_id: user.id).exists?
   end
