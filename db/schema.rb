506b9624d2fd0d5da5364202c3a5bb0f54ee67c9
takumi <tkm0703@icloud.com>
2021-05-05 14:20:34 +0900

    db修正

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_04_21_005155) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
     t.integer "study_id"
