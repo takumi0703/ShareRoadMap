f8edb61fb788e1cd0544c9fa48f7f5460822b85c
takumi <tkm0703@icloud.com>
2021-05-07 01:41:59 +0900

    aboutページ削除、header修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails
   .application
   .routes
   .draw do
+    get '/' => 'user#new'
     get 'login' => 'user#login_form'
     post 'login' => 'user#login'
     get 'logout' => 'user#destroy'
 
     get 'user/index' => 'user#index'
-    get '/' => 'user#about'
     post 'user/create' => 'user#create'
     post 'user/:id/update' => 'user#update'
     get 'user/road/:id' => 'user#roadshow'
