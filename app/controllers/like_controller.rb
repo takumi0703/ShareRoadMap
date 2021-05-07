7ee781d28ce43af693077a8751a72afbc0511fa3
takumi <tkm0703@icloud.com>
2021-05-07 20:34:34 +0900

    like_controllerのリファクタリング

--- app/controllers/like_controller.rb
+++ app/controllers/like_controller.rb
@@ -1,16 +1,22 @@
 class LikeController < ApplicationController
+  before_action :set_like_study, { only: %i[create destroy] }
+
   def create
-    study = Study.find_by(id: params[:study_id])
     like = @current_user.likes.build(study_id: params[:study_id])
     like.save
     flash[:success] = 'いいねしました！'
-    redirect_to("/user/road/#{study.user_id}")
+    redirect_to("/user/road/#{@study.user_id}")
   end
 
   def destroy
-    study = Study.find_by(id: params[:study_id])
     like = Like.find_by(study_id: params[:study_id], user_id: @current_user.id)
     like.delete
-    redirect_to("/user/road/#{study.user_id}")
+    redirect_to("/user/road/#{@study.user_id}")
+  end
+
+  private
+
+  def set_like_study
+    @study = Study.find_by(id: params[:study_id])
   end
 end
