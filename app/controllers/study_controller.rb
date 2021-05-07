7cd317830a1aed6be1d2d0538ee775e985056213
takumi <tkm0703@icloud.com>
2021-05-07 20:29:25 +0900

    study_controllerのリファクタリング

--- app/controllers/study_controller.rb
+++ app/controllers/study_controller.rb
@@ -4,6 +4,7 @@ class StudyController < ApplicationController
   before_action :autheniticate_user, { only: %i[new edit completed_create completed_destroy] }
   before_action :not_set_current_user_study, { only: [:edit] }
   before_action :not_set_current_user_complete, { only: %i[completed_create completed_destroy] }
+  before_action :set_study, { only: %i[completed_create completed_destroy edit update destroy] }
   def new
     @studies = Study.new
   end
@@ -19,24 +20,19 @@ class StudyController < ApplicationController
   end
 
   def completed_create
-    @study = Study.find(params[:id])
     completed = @study.update(completed: @current_user.id)
     flash[:success] = '達成おめでとう！!'
     redirect_to("/user/road/#{@study.user_id}")
   end
 
   def completed_destroy
-    @study = Study.find(params[:id])
     completed = @study.update(completed: nil)
     redirect_to("/user/road/#{@study.user_id}")
   end
 
-  def edit
-    @study = Study.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @study = Study.find(params[:id])
     @study.update(study_params)
     if @study.save
       flash[:success] = '学習を編集しました！'
@@ -47,7 +43,6 @@ class StudyController < ApplicationController
   end
 
   def destroy
-    @study = Study.find(params[:id])
     if @study.destroy
       flash[:success] = '学習を削除しました！'
       redirect_to("/user/road/#{@study.user_id}")
@@ -55,6 +50,10 @@ class StudyController < ApplicationController
   end
 
   private
+
+  def set_study
+    @study = Study.find(params[:id])
+  end
 
   def study_params
     params.permit(:content, :material, :period).merge(user_id: @current_user.id)
