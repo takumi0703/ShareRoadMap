3e4ffae5f8328b93d1353df9d814303473cb418a
takumi <tkm0703@icloud.com>
2021-05-11 00:10:29 +0900

    commentとlikeの初期テストの削除

--- spec/factories/comments.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :comment do
-    comment_content { 'MyString' }
-    user { nil }
-    study { nil }
-  end
-end
--- spec/factories/likes.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :like do
-    user_id { 1 }
-    study_id { 1 }
-  end
-end
