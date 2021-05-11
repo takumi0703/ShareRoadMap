1588228750b8965b49e56f0fa15c1da81570c607
takumi <tkm0703@icloud.com>
2021-05-11 14:08:00 +0900

    studyモデルテスト作成

--- spec/models/study_spec.rb
+++ spec/models/study_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Study, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'study新規作成' do
+    context '正常な値の時' do
+      context '全ての値が正しく入力されている時' do
+        it '有効な状態であること' do
+          study = FactoryBot.build(:study)
+          expect(study).to be_valid
+        end
+      end
+    end
+    context '不正な値の時' do
+      context 'contentが空の時' do
+        it '無効な状態であること' do
+          study = FactoryBot.build(:study, content: nil)
+          expect(study).to_not be_valid
+        end
+      end
+      context 'user_idが空の時' do
+        it '無効な状態であること' do
+          study = FactoryBot.build(:study, user_id: nil)
+          expect(study).to_not be_valid
+        end
+      end
+    end
+  end
 end
