26efd6c2a4bbb01d5628f457b06c98c8fbe4079e
takumi <tkm0703@icloud.com>
2021-05-12 14:19:33 +0900

    requestスペック作成

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe 'GET /' do
+  describe '#new' do
     it '正常にレスポンスを返すこと' do
       get '/'
       expect(response).to have_http_status(200)
+    end
+  end
+  describe '#index' do
+    context 'ログインしていない時' do
+      it 'ログイン画面にリダイレクトされていること' do
+        get user_index_path
+        expect(response).to redirect_to login_path
+      end
+    end
+    context 'ログイン済みの時' do
+      it '正常にレスポンスを返すこと' do
+        get user_index_path
+        expect(response).to have_http_status(200)
+      end
     end
   end
 end
