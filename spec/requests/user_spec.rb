f054b21c351c4eb7efa1979b593cb11b1194c489
takumi <tkm0703@icloud.com>
2021-05-12 19:12:49 +0900

    UserRequestスペック　リファクタリング

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -1,44 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe '未ログイン' do
-    context '#new' do
+  describe '#new' do
+    context '未ログイン' do
       it '正常にレスポンスを返すこと' do
         get '/'
         expect(response).to have_http_status(200)
       end
     end
-    context '#index' do
-    it 'ログイン画面にリダイレクトされていること' do
-      get user_index_path
-      expect(response).to redirect_to login_path
+  end
+  describe '#index' do
+    context '未ログイン' do
+      it 'ログイン画面にリダイレクトされていること' do
+        get user_index_path
+        expect(response).to redirect_to login_path
       end
     end
-    context '#show' do
+    context 'ログイン時' do
+       # it '正常にレスポンスを返すこと' do
+        #   get user_index_path
+        #   expect(response).to have_http_status(200)
+        # end
+    end
+  end
+  describe '#show' do
+    context '未ログイン' do
       it 'ログイン画面にリダイレクトされていること' do
         get '/user/:id'
         expect(response).to redirect_to login_path
       end
     end
-    context '#edit' do
+  end
+  describe '#edit' do
+    context '未ログイン' do
       it 'ログイン画面にリダイレクトされていること' do
         get '/user/:id/edit'
         expect(response).to redirect_to login_path
       end
     end
-    context '#roadshow' do
+  end
+  describe '#roadshow' do
+    context '未ログイン' do
       it 'ログイン画面にリダイレクトされていること' do
         get '/user/road/:id'
         expect(response).to redirect_to login_path
       end
-    end
-  end
-  describe 'ログイン済' do
-    context '#index' do
-        # it '正常にレスポンスを返すこと' do
-        #   get user_index_path
-        #   expect(response).to have_http_status(200)
-        # end
     end
   end
 end
