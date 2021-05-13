4f1f7732a42dee9c0be54fb5a5ca8b28be1967df
takumi <tkm0703@icloud.com>
2021-05-13 20:26:06 +0900

    user system spec　リファクタリング

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -13,37 +13,35 @@ RSpec.describe 'Users', type: :system do
         end
     end
     describe 'ログイン' do
+        before do
+            @user = FactoryBot.create(:user)
+            login_in_as @user
+        end
         context '入力が正しい時' do
-            before do
-                @user = FactoryBot.create(:user)
-                login_in_as @user
-            end
             it 'ログインのフラッシュが表示されていること' do
                 expect(page).to have_content 'ログインしました'
             end
         end
-    end
-    describe 'ユーザー編集' do
-        before do
-            @user = FactoryBot.create(:user)
-            login_in_as @user
-            visit edit_user_path(@user)
-        end
-        context '入力が正しい時' do
-            it '編集完了のフラッシュが表示されていること' do
-                fill_in "name", with: 'test-name'
-                attach_file "image_icon", "app/assets/images/default.jpeg"
-                fill_in "email", with: 'test@example.com'
-                fill_in "goal", with: 'test-goal'
-                click_button "保存"
-                expect(page).to have_content 'ユーザー情報を編集できました！'
+        describe 'ユーザー編集' do
+            before do
+                visit edit_user_path(@user)
             end
-        end
-        context 'ユーザー名が空の時' do
-            it 'エラーメッセージが表示されていること' do
-                fill_in "name", with: ''
-                click_button "保存"
-                expect(page).to have_content '編集に誤りがあります'
+            context '入力が正しい時' do
+                it '編集完了のフラッシュが表示されていること' do
+                    fill_in "name", with: 'test-name'
+                    attach_file "image_icon", "app/assets/images/default.jpeg"
+                    fill_in "email", with: 'test@example.com'
+                    fill_in "goal", with: 'test-goal'
+                    click_button "保存"
+                    expect(page).to have_content 'ユーザー情報を編集できました！'
+                end
+            end
+            context 'ユーザー名が空の時' do
+                it 'エラーメッセージが表示されていること' do
+                    fill_in "name", with: ''
+                    click_button "保存"
+                    expect(page).to have_content '編集に誤りがあります'
+                end
             end
         end
     end
