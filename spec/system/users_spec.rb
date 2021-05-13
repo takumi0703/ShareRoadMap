8c4249ccd17716d38d168b218888973b5221981b
takumi <tkm0703@icloud.com>
2021-05-13 11:45:21 +0900

    user systemspec作成

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -37,19 +37,20 @@ RSpec.describe 'Users', type: :system do
             fill_in "email", with: @user.email
             fill_in "password", with: @user.password
             click_button "ログイン"
+            visit edit_user_path(@user)
         end
         context '入力が正しい時' do
-            it 'ユーザー名が編集されていること' do
-                visit edit_user_path(@user)
+            it '編集完了のフラッシュが表示されていること' do
                 fill_in "name", with: 'test-name'
+                attach_file "image_icon", "app/assets/images/default.jpeg"
+                fill_in "email", with: 'test@example.com'
+                fill_in "goal", with: 'test-goal'
                 click_button "保存"
-                expect(page).to have_content 'test-name'
                 expect(page).to have_content 'ユーザー情報を編集できました！'
             end
         end
         context 'ユーザー名が空の時' do
             it 'エラーメッセージが表示されていること' do
-                visit edit_user_path(@user)
                 fill_in "name", with: ''
                 click_button "保存"
                 expect(page).to have_content '編集に誤りがあります'
