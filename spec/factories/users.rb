cf131c8d4dbf0e7f6bd06c934c28bd4eb751ba61
takumi <tkm0703@icloud.com>
2021-05-23 16:36:02 +0900

    Model user_spec リファクタリング

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,6 +7,16 @@ FactoryBot.define do
       sequence(:email) { 'test@example.com' }
       password { 'password' }
     end
+    trait :already_name do
+      name { 'username' }
+      sequence(:email) { 'test1@example.com' }
+      password { 'password' }
+    end
+    trait :already_email do
+      name { 'username1' }
+      sequence(:email) { 'test@example.com' }
+      password { 'password' }
+    end
     trait :user2 do
       name { 'username2' }
       sequence(:email) { 'test2@example.com' }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,16 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'user新規登録' do
-    context '正常な値の時' do
-      context '全ての値が正しく入力されている時' do
-        it '有効な状態であること' do
-          user = build(:user, :user1)
-          expect(user).to be_valid
-        end
+  describe '新規登録' do
+    context '全ての値が正しく入力されている時' do
+      it '有効な状態であること' do
+        user = build(:user, :user1)
+        expect(user).to be_valid
       end
     end
-    context '不正な値の時' do
+    context '不正な値' do
       context 'nameが空の時' do
         it 'nameにエラーが含まれていること' do
           user = build(:user, :user1, name: nil)
@@ -28,40 +26,26 @@ RSpec.describe User, type: :model do
         end
       end
       context 'passwordが空の時' do
-        it 'nameにエラーが含まれていること' do
+        it 'passwordにエラーが含まれていること' do
           user = build(:user, :user1, password: nil)
           user.valid?
           expect(user.errors[:password]).to include('を入力してください')
         end
       end
       context '値が重複する時' do
-        before do
-          User.create(
-            name: 'test',
-            email: 'test@example.com',
-            password: 'test'
-          )
-        end
+        let!(:user) {create(:user,:user1)}
         context 'nameの値が重複する時' do
+          let(:already_name) {build(:user,:already_name)}
           it 'nameにエラーが含まれていること' do
-            user = User.new(
-              name: 'test',
-              email: 'test1@example.com',
-              password: 'test1'
-            )
-            user.valid?
-            expect(user.errors[:name]).to include('はすでに存在します')
+            already_name.valid?
+            expect(already_name.errors[:name]).to include('はすでに存在します')
           end
         end
         context 'emailの値が重複する時' do
+          let(:already_email) {build(:user,:already_email)}
           it 'emailにエラーが含まれていること' do
-            user = User.new(
-              name: 'test1',
-              email: 'test@example.com',
-              password: 'test1'
-            )
-            user.valid?
-            expect(user.errors[:email]).to include('はすでに存在します')
+            already_email.valid?
+            expect(already_email.errors[:email]).to include('はすでに存在します')
           end
         end
       end
