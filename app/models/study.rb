3866d1b211e24f40d306aecac686cdc146e98540
takumi <tkm0703@icloud.com>
2021-05-24 23:52:37 +0900

    モデルの関連づけ

--- app/models/study.rb
+++ app/models/study.rb
@@ -5,6 +5,8 @@ class Study < ApplicationRecord
   validates :user_id, presence: true
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :tag_maps, dependent: :destroy
+  has_many :tags, through: :tag_maps#投稿に紐づけられたタグを取得することができる
   scope :order_asc, -> { order(id: :asc) }
 
   def liked_by?(user)
