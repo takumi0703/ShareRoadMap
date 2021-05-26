e4d14b1274af6b80d10bc40ffb8654d45bcd4069
takumi <tkm0703@icloud.com>
2021-05-26 19:58:04 +0900

    tag systemspec　修正

--- spec/system/tags_spec.rb
+++ spec/system/tags_spec.rb
@@ -8,29 +8,35 @@ RSpec.describe 'Tags', type: :system do
         visit new_study_path
         fill_in 'content', with: '投稿'
     end
-    it 'タグが大文字で入力された場合でも、小文字で表示されていること' do
-        fill_in 'tag_name', with: 'RAILS'
-        click_button '作成'
-        user_road_path(user)
-        expect(page).to have_content 'rails'
+    describe '新規登録' do
+        it 'タグが大文字で入力された場合でも、小文字で表示されていること' do
+            fill_in 'tag_name', with: 'RAILS'
+            click_button '作成'
+            user_road_path(user)
+            expect(page).to have_content 'rails'
+        end
     end
-    it 'タグ一覧のタグをクリックされた時、そのタグ名が表示されていること' do
-        fill_in 'tag_name', with: 'rails'
-        click_button '作成'
-        visit study_index_path
-        expect(page).to have_content 'rails'
-    end
-    context 'タグ絞り込み' do
-        it 'タグをクリックされた時、そのタグに関連していない学習は表示されないこと' do
+    describe '学習投稿一覧' do
+        before do
             fill_in 'tag_name', with: 'rails'
             click_button '作成'
-            visit new_study_path
-            fill_in 'content', with: 'phpの学習投稿'
-            fill_in 'tag_name', with: 'php'
-            click_button '作成'
-            visit study_index_path
-            click_link 'rails'
-            expect(page).to_not have_content 'phpの学習投稿'
+        end
+        context 'クリックされた時' do
+            it 'そのタグ名が表示されていること' do
+                visit study_index_path
+                expect(page).to have_content 'rails'
+            end
+        end
+        context 'タグ絞り込み' do
+            it 'タグをクリックされた時、そのタグに関連していない学習は表示されないこと' do
+                visit new_study_path
+                fill_in 'content', with: 'phpの学習投稿'
+                fill_in 'tag_name', with: 'php'
+                click_button '作成'
+                visit study_index_path
+                click_link 'rails'
+                expect(page).to_not have_content 'phpの学習投稿'
+            end
         end
     end
     describe '編集' do
@@ -39,15 +45,16 @@ RSpec.describe 'Tags', type: :system do
             fill_in 'tag_name', with: 'rails'
             click_button '作成'
             click_on '編集'
-        end
-        it '正しく編集された時、そのタグが表示されていること' do
             fill_in 'tag_name', with: 'ruby'
             click_button '保存'
-            expect(page).to have_content 'ruby'
         end
-        it '編集された時、削除されたタグは表示されていないこと' do
-            fill_in 'tag_name', with: 'ruby'
-            click_button '保存'
+        context '正しく編集された時' do
+            it 'そのタグが表示されていること' do
+                expect(page).to have_content 'ruby'
+            end
+        end
+        context 'タグ名を変更した時'
+        it '削除されたタグは表示されていないこと' do
             expect(page).to_not have_content 'rails'
         end
     end
