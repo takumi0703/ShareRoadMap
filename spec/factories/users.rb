1b97f58b667ed44a915eb61438c120ca06623c38
takumi <tkm0703@icloud.com>
2021-05-26 17:06:13 +0900

    factorybot users修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,17 +4,17 @@ FactoryBot.define do
   factory :user do
     trait :user1 do
       name { 'username' }
-      sequence(:email) { 'test@example.com' }
+      email { 'test@example.com' }
       password { 'password' }
     end
     trait :already_email do
       name { 'username1' }
-      sequence(:email) { 'test@example.com' }
+      email { 'test@example.com' }
       password { 'password' }
     end
     trait :user2 do
       name { 'username2' }
-      sequence(:email) { 'test2@example.com' }
+      email { 'test2@example.com' }
       password { 'password' }
     end
   end
