089bfbefb83ca7617e7d7a7df84362e13d53a50a
takumi <tkm0703@icloud.com>
2021-05-26 18:56:19 +0900

    tag model spec作成

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
+    it 'タグが正しく入力された時、有効な状態であること' do
+        tag = build(:tag)
+        tag.valid?
+        expect(tag).to be_valid
+    end
+    it 'タグが複数入力された時、配列の数が2つになっていること' do
+        tag = build(:tag,:tag_list)
+        tag.valid?
+        num = tag.tag_name.split(/[[:blank:]]+/)
+        expect(num.size).to eq 2
+    end
 end
